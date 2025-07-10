80428f5a65ccc3fdbdceb48139eff9e846b265ac
JCordry <julien.cordry@gmail.com>
2025-07-10 14:31:54 +0100

    Show the map

---
+++ assets/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tiles" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="tiles.png" width="96" height="96"/>
+</tileset>
--- core/src/main/kotlin/uk/ac/scedt/mam/map/Main.kt
+++ core/src/main/kotlin/uk/ac/scedt/mam/map/Main.kt
@@ -1,8 +1,14 @@
 package uk.ac.scedt.mam.map
 
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.Texture.TextureFilter.Linear
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.maps.tiled.TiledMap
+import com.badlogic.gdx.maps.tiled.TmxMapLoader
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer
+import com.badlogic.gdx.utils.viewport.FitViewport
 import ktx.app.KtxGame
 import ktx.app.KtxScreen
 import ktx.app.clearScreen
@@ -15,24 +21,50 @@ class Main : KtxGame<KtxScreen>() {
     override fun create() {
         KtxAsync.initiate()
 
-        addScreen(FirstScreen())
-        setScreen<FirstScreen>()
+        addScreen(MapScreen())
+        setScreen<MapScreen>()
     }
 }
 
-class FirstScreen : KtxScreen {
-    private val image = Texture("logo.png".toInternalFile(), true).apply { setFilter(Linear, Linear) }
+class MapScreen : KtxScreen {
+//    private val image = Texture("logo.png".toInternalFile(), true).apply { setFilter(Linear, Linear) }
     private val batch = SpriteBatch()
+    private lateinit var map: TiledMap
+    private lateinit var renderer: OrthogonalTiledMapRenderer
+    private lateinit var camera: OrthographicCamera
+    private lateinit var viewport: FitViewport // Is this correct?
+
+    override fun show() {
+        super.show()
+        camera = OrthographicCamera()
+        viewport = FitViewport(800f, 600f, camera)
+        viewport.apply()
+        Gdx.files.internal("tilemap01.tmx")
+        val mapLoader = TmxMapLoader()
+        map = mapLoader.load("tilemap01.tmx")
+        renderer = OrthogonalTiledMapRenderer(map, 1f)
+//        camera.setToOrtho(true)
+        camera.position.set(viewport.worldWidth/2f, viewport.worldHeight/2f, 0f)
+    }
 
     override fun render(delta: Float) {
-        clearScreen(red = 0.7f, green = 0.7f, blue = 0.7f)
+        clearScreen(red = 0f, green = 0f, blue = 0f)
+        camera.update()
+        renderer.setView(camera)
+        renderer.render()
         batch.use {
-            it.draw(image, 100f, 160f)
+//            it.draw(image, 100f, 160f)
         }
     }
 
+    override fun resize(width: Int, height: Int) {
+        super.resize(width, height)
+        viewport.update(width, height)
+    }
+
     override fun dispose() {
-        image.disposeSafely()
+//        image.disposeSafely()
+        renderer.disposeSafely()
         batch.disposeSafely()
     }
 }
